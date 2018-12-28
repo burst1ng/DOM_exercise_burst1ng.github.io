82944c435dffade3907ac0b28be53eb2aed8a930
burst1ng <44242951+burst1ng@users.noreply.github.com>
2018-12-28 13:00:49 -0800

    uploading an app
    a primitive todo-list application from Andrei's Exercise DOM Events

---
+++ script.js
@@ -0,0 +1,57 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+
+	var deleteBtn = document.createElement("button");
+	deleteBtn.appendChild(document.createTextNode("delete"));//create a button named "delete"
+	li.appendChild(deleteBtn);
+	deleteBtn.addEventListener("click", deleteItem);//others code with onclick event as follows: deleteBtn.onclick = deleteItem;
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function crossItem(event) {
+	if (event.target !== event.currentTarget) {
+		event.target.classList.toggle("done");
+	}//event bubbling and handler accessing elements
+}
+
+function deleteItem(event) {
+	if (event.target && event.target.parentNode !== event.currentTarget) {
+		event.target.parentNode.remove();
+	}//same event bubbling
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", crossItem, deleteItem);//add two functions here and works
+
+
+
+
+
+
+
